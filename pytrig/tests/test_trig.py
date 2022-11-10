c2b5b7817877fed97e93998a0ff0be3b64dc212c
Jacob Lee <JLpython@outlook.com>
2022-11-10 08:11:09 -0800

    test: pytrig.trig.tangent

--- pytrig/tests/test_trig.py
+++ pytrig/tests/test_trig.py
@@ -19,23 +19,23 @@ D = decimal.Decimal
 
 @pytest.mark.parametrize(
     "x, value", [
-        (-2 * PI, D(0)),
+        (-2 * PI, 0),
         (-11 * PI / 6, 1 / D(2)), (-7 * PI / 4, D(2).sqrt() / 2), (-5 * PI / 3, D(3).sqrt() / 2),
-        (-3 * PI / 2, D(1)),
+        (-3 * PI / 2, 1),
         (-4 * PI / 3, D(3).sqrt() / 2), (-5 * PI / 4, D(2).sqrt() / 2), (-7 * PI / 6, 1 / D(2)),
-        (-PI, D(0)),
+        (-PI, 0),
         (-5 * PI / 6, -1 / D(2)), (-3 * PI / 4, -D(2).sqrt() / 2), (-2 * PI / 3, -D(3).sqrt() / 2),
-        (-PI / 2, -D(1)),
+        (-PI / 2, -1),
         (-PI / 3, -D(3).sqrt() / 2), (-PI / 4, -D(2).sqrt() / 2), (-PI / 6, -1 / D(2)),
-        (D(0), D(0)),
+        (D(0), 0),
         (PI / 6, 1 / D(2)), (PI / 4, D(2).sqrt() / 2), (PI / 3, D(3).sqrt() / 2),
-        (PI / 2, D(1)),
+        (PI / 2, 1),
         (2 * PI / 3, D(3).sqrt() / 2), (3 * PI / 4, D(2).sqrt() / 2), (5 * PI / 6, 1 / D(2)),
-        (PI, D(0)),
+        (PI, 0),
         (7 * PI / 6, -1 / D(2)), (5 * PI / 4, -D(2).sqrt() / 2), (4 * PI / 3, -D(3).sqrt() / 2),
-        (3 * PI / 2, -D(1)),
+        (3 * PI / 2, -1),
         (5 * PI / 3, -D(3).sqrt() / 2), (7 * PI / 4, -D(2).sqrt() / 2), (11 * PI / 6, -1 / D(2)),
-        (2 * PI, D(0))
+        (2 * PI, 0)
     ]
 )
 def test_sine(x: D, value: D):
@@ -51,23 +51,23 @@ def test_sine(x: D, value: D):
 
 @pytest.mark.parametrize(
     "x, value", [
-        (-2 * PI, D(1)),
+        (-2 * PI, 1),
         (-11 * PI / 6, D(3).sqrt() / 2), (-7 * PI / 4, D(2).sqrt() / 2), (-5 * PI / 3, 1 / D(2)),
         (-3 * PI / 2, D(0)),
         (-4 * PI / 3, -1 / D(2)), (-5 * PI / 4, -D(2).sqrt() / 2), (-7 * PI / 6, -D(3).sqrt() / 2),
-        (-PI, -D(1)),
+        (-PI, -1),
         (-5 * PI / 6, -D(3).sqrt() / 2), (-3 * PI / 4, -D(2).sqrt() / 2), (-2 * PI / 3, -1 / D(2)),
-        (-PI / 2, D(0)),
+        (-PI / 2, 0),
         (-PI / 3, 1 / D(2)), (-PI / 4, D(2).sqrt() / 2), (-PI / 6, D(3).sqrt() / D(2)),
-        (D(0), D(1)),
+        (D(0), 1),
         (PI / 6, D(3).sqrt() / 2), (PI / 4, D(2).sqrt() / 2), (PI / 3, 1 / D(2)),
-        (PI / 2, D(0)),
+        (PI / 2, 0),
         (2 * PI / 3, -1 / D(2)), (3 * PI / 4, -D(2).sqrt() / 2), (5 * PI / 6, -D(3).sqrt() / 2),
-        (PI, -D(1)),
+        (PI, -1),
         (7 * PI / 6, -D(3).sqrt() / 2), (5 * PI / 4, -D(2).sqrt() / 2), (4 * PI / 3, -1 / D(2)),
-        (3 * PI / 2, D(0)),
+        (3 * PI / 2, 0),
         (5 * PI / 3, 1 / D(2)), (7 * PI / 4, D(2).sqrt() / 2), (11 * PI / 6, D(3).sqrt() / D(2)),
-        (2 * PI, D(1))
+        (2 * PI, 1)
     ]
 )
 def test_cosine(x: D, value: D):
@@ -80,3 +80,29 @@ def test_cosine(x: D, value: D):
     res = trig.cosine(x, PRECISION)
     assert abs(res - value) <= D(10) ** -(PRECISION - 1), (x, (res, value))
 
+
+@pytest.mark.parametrize(
+    "x, value", [
+        (-2 * PI, 0),
+        (-3 * PI / 2, INF),
+        (-PI, 0),
+        (-PI / 2, NINF),
+        (0, 0),
+        (PI / 2, INF),
+        (PI, 0),
+        (3 * PI / 2, NINF),
+        (2 * PI, 0),
+    ]
+)
+def test_tangent(x: D, value: D):
+    """
+
+    :param x:
+    :param value:
+    :return:
+    """
+    res = trig.tangent(x, PRECISION)
+    if value is INF or value is NINF:
+        assert res is value
+    else:
+        assert abs(res - value) <= D(10) ** -(PRECISION - 1), (x, (res, value))
